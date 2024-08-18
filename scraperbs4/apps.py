2ba5e2343e16fcf21f08c5b9718975764d67bb82
Sana Mousavi <sanamousavie@gmail.com>
2024-08-18 12:44:34 +0330

    Delete scraperbs4 directory

--- scraperbs4/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Scraperbs4Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scraperbs4'
--- scraperbs4/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import populate_database
-
-urlpatterns = [
-    path('', populate_database)
-]

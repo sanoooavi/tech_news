49e20c2dad796484bcabcf887cb1677ffaefd23f
sanoooavi <sanamousavie@gmail.com>
2024-08-24 01:14:51 +0330

    changed minutes for test

--- tech_news/celery.py
+++ tech_news/celery.py
@@ -11,7 +11,7 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'run-scraper-every-day': {
         'task': 'scraperbs4.tasks.run_scraper',
-        'schedule': timedelta(hours=12),
+        'schedule': timedelta(minutes=5),
         'args': (1, 10)
     }
 
